96debbeecdf2ee44c97710175d024f4aee1ad31c
Sean <974603X@student.swin.edu.au>
2017-05-17 14:39:04 +1000

    First Initial push for project. Working file input and output for testing.

---
+++ Assignment2/Assignment2/FileIO.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment2
+{
+    class FileIO
+    {
+        private StreamReader sr;
+        private string line;
+        private List<string> StoredLines;
+
+        public FileIO(string Fname)
+        {
+            StoredLines = new List<string>();
+            Open(Fname);
+        }
+
+        private bool Open(string Fname)
+        {
+            bool Result = false;
+
+            try
+            {
+                using (sr = new StreamReader(Fname))
+                {
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        string[] seperatedText = line.Split(';');
+                        foreach (string l in seperatedText)
+                        {
+                            StoredLines.Add(l);
+                        }
+                    }
+                    Result = true;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("File not found");
+                Console.WriteLine(e.Message);
+                Result = false;
+            }
+
+            return Result;
+        }
+
+        public void ExpelStoredLines()
+        {
+            for(int i = 0; i < StoredLines.Count; i++)
+            {
+                Console.WriteLine(StoredLines[i]);
+                //Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileIO Fio = new FileIO(args[1]);
+
+            if (args[0] == "FIO")
+            {
+                Fio.ExpelStoredLines();
+            }
+        }
+    }
+}
